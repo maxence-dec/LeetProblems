26637252f892218045ddf5cb267383e127c81229
maxence-dec <mp.decourriere@pm.me>
2026-03-04 19:30:32 +0100

    spring cleaning 3/3

--- 0175. Combine Two Tables/CombineTwoTables.sql
+++
@@ -1,5 +0,0 @@
-select
-    p.lastName, p.firstName, a.city, a.state
-from
-    Person as p
-    left join Address as a on p.personId = a.personId
--- 0392. Is Subsequence/IsSubsequence.c
+++
@@ -1,8 +0,0 @@
-bool isSubsequence(char* s, char* t) {
-    int cptS = 0;
-    int tLen = strlen(t);
-    for(int i = 0; i < tLen; i++)
-        if(s[cptS] == t[i]) cptS++;
-
-    return cptS == strlen(s);
-}
